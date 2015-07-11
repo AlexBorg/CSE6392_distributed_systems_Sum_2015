c6208cb1a3f295dfb9fe49afb7c4e6ee7f0847ae
AlexBorg <borg@alumni.utexas.net>
2015-07-11 12:55:56 -0500

    Move messages to named tuples
    Move message to named tuples and update gitignore for python files.

--- messages.py
+++ messages.py
@@ -1,29 +1,16 @@
 # this file contains all the messages to be sent between the client and the server
 # It also contains connection port information and any defines needed by both
 
+import collections
+
 ANNOUNCE_PORT = 2000
 CONNECTION_PORT = 2001
 
+MessageData = collections.namedtuple('MessageData', 'group message')
 
-class MessageData:
-    def __init__(self, group, message):
-        self.group = group
-        self.message = message
+LoginData = collections.namedtuple('LoginData', 'username password')
 
+# join is true for joining, and false for leaving
+GroupSubscriptionData = collections.namedtuple('GroupSubscriptionData', 'group join')
 
-class LoginData:
-    def __init__(self, username="", password=""):
-        self.username = username
-        self.password = password
-
-
-class GroupSubscriptionData:
-    def __init__(self, group="", join=True):
-        self.group = group
-        self.join = join  # join is true for joining, and fasle for leaving
-
-
-class AnnouncementData:
-    def __init__(self, address="", name=""):
-        self.address = address
-        self.name = name
+AnnouncementData = collections.namedtuple('AnnouncementData', 'address name')
